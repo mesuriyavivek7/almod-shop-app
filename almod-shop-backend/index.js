9472ea526fd0ebf4a01fd6b438bd647743b5c474
kakadiyapalak <kakadiyapalak27@gmail.com>
2024-08-31 18:34:47 +0530

    navbar

--- almod-shop-backend/index.js
+++ almod-shop-backend/index.js
@@ -13,6 +13,8 @@ import instaRoute from './routes/insta.js'
 import categoryRoute from './routes/pcategory.js'
 import transactionRoute from './routes/transac.js'
 import mailRoute from './routes/mail.js'
+import careerRoute from './routes/career.js'
+import applicationRoute from './routes/application.js'
 
 
 import cookieParser from "cookie-parser"
@@ -20,15 +22,15 @@ import cookieParser from "cookie-parser"
 dotenv.config()
 
 
-const app=express()
+const app = express()
 
 
 
 
 //cors settings
-const corsOptions={
-    origin:(origin,callback)=>{
-        const allowedOrigins=[
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = [
             "http://localhost:3000",
             "https://fuelflex.in",
             "https://www.fuelflex.in"
@@ -51,56 +53,58 @@ app.use(cookieParser())
 
 //connecting to mongodb server
 
-const connectDb=async ()=>{
+const connectDb = async () => {
 
-    try{
-       await mongoose.connect(process.env.MONGO)
-       console.log("Connected to mongodb sucessfully")
-    }catch(err){
-       throw err
+    try {
+        await connect(process.env.MONGO)
+        console.log("Connected to mongodb sucessfully")
+    } catch (err) {
+        throw err
     }
 }
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Bahut Maja A raha hai🤓")
 })
 
 //to notify mongodb is connected or disconnected
-mongoose.connection.on("connected",()=>{
+mongoose.connection.on("connected", () => {
     console.log("Mongodb connected sucessfully")
 })
 
-mongoose.connection.on("disconnected",()=>{
+mongoose.connection.on("disconnected", () => {
     console.log("Mongodb disconnected")
 })
 
 
 
 //middleware 
-app.use('/api/auth',authRoute)
-app.use('/api/user',userRoute)
-app.use('/api/product',productRoute)
-app.use('/api/payment',paymentRoute)
-app.use('/api/insta',instaRoute)
-app.use('/api/category',categoryRoute)
-app.use('/api/transaction',transactionRoute)
-app.use('/api/mail',mailRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/user', userRoute)
+app.use('/api/product', productRoute)
+app.use('/api/payment', paymentRoute)
+app.use('/api/insta', instaRoute)
+app.use('/api/category', categoryRoute)
+app.use('/api/transaction', transactionRoute)
+app.use('/api/mail', mailRoute)
+app.use('/api/career', careerRoute)
+app.use('/api/application', applicationRoute)
 
 //middleware for catch error
 
-app.use((err,req,res,next)=>{
-   const errStatus=err.status || 500
-   const errMsg=err.message || "Something went wrong!"
+app.use((err, req, res, next) => {
+    const errStatus = err.status || 500
+    const errMsg = err.message || "Something went wrong!"
 
-   return res.status(errStatus).json({
-       success:'false',
-       status:errStatus,
-       message:errMsg,
-       stack:err.stack
-   })
+    return res.status(errStatus).json({
+        success: 'false',
+        status: errStatus,
+        message: errMsg,
+        stack: err.stack
+    })
 })
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     connectDb()
     console.log("app is listented on port:8080")
 })
--- almod-shop-backend/models/Product.js
+++ almod-shop-backend/models/Product.js
@@ -1,41 +1,41 @@
 import mongoose from "mongoose";
 
-const productSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    type:{
-        type:String,
-        required:true
+    type: {
+        type: String,
+        required: true
     },
-    size:{
-        type:String,
-        required:true
+    size: {
+        type: String,
+        required: true
     },
-    category:{
-        type:String,
-        required:true
+    category: {
+        type: String,
+        required: true
     },
-    price:{
-        type:Number,
-        required:true
+    price: {
+        type: Number,
+        required: true
     },
-    discount:{
-        type:Number
+    discount: {
+        type: Number
     },
-    images:{
-        type:[String]
+    images: {
+        type: [String]
     },
-    desc:{
-        type:String,
-        required:true
+    desc: {
+        type: String,
+        required: true
     },
-    rating:{
-        type:Number
+    rating: {
+        type: Number
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 })
 
-export default mongoose.model("Product",productSchema)
+export default mongoose.model("Product", productSchema)
